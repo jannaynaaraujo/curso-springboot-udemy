43f39f4b6d0b3bf59e509c509d3969504186b183
Jannayna Araujo <contatojannayna@gmail.com>
2020-08-15 12:57:08 -0300

    Testando primeira classe de dominio (Category)

--- src/main/java/com/udemy/cursospring/resources/CategoryResource.java
+++ src/main/java/com/udemy/cursospring/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.udemy.cursospring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.cursospring.domain.Category;
 
 @RestController
 @RequestMapping(value = "categories")
 public class CategoryResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String getCategories() {
-		return "GetCategories works!";
+	public List<Category> getCategories() {
+		
+		Category category1 = new Category(1, "Informatica"); 
+		Category category2 = new Category(2, "Escritorio"); 
+		
+		List<Category> categories = new ArrayList<>();
+		categories.add(category1);
+		categories.add(category2);
+		
+		return categories;
 	}
 
 }
