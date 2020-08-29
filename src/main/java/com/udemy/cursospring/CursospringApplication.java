f39095a006e2e24d0fe3812586c505e3293689d1
Jannayna Araujo <contatojannayna@gmail.com>
2020-08-28 23:32:52 -0300

    Criando lista de categorias automaticamente na inicialização

--- src/main/java/com/udemy/cursospring/CursospringApplication.java
+++ src/main/java/com/udemy/cursospring/CursospringApplication.java
@@ -1,13 +1,32 @@
 package com.udemy.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.udemy.cursospring.domain.Category;
+import com.udemy.cursospring.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category informatica = new Category(null, "Informática");
+		Category escritorio = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(informatica, escritorio));
+		
 	}
 
 }
